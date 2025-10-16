e1332564bdc3b458dd63c9ee3b4f9bacae933385
Niki Chung <nikic5409@gmail.com>
2025-10-16 11:55:30 -0700

    wow

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -2,36 +2,56 @@ package com.example.quizapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.OnApplyWindowInsetsListener
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.quizapp.PracticingJsonActivity.Companion.TAG
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    private lateinit var questions:TextView
+    private lateinit var option1: Button
+    private lateinit var option2: Button
+    private lateinit var option3: Button
+    private lateinit var option4: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        this.enableEdgeToEdge()
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(
-            findViewById<View?>(R.id.main),
-            OnApplyWindowInsetsListener { v: View?, insets: WindowInsetsCompat? ->
-                val systemBars = insets!!.getInsets(WindowInsetsCompat.Type.systemBars())
-                v!!.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-                insets
-            })
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        wireWidgets()
+
 
         val gson = Gson()
         val inputStream = resources.openRawResource(R.raw.quiz)
         val jsonString = inputStream.bufferedReader().use {
-            //kotlin use lambda makes "it" the object use is called on
-            //will close that resource when the block finishes
             it.readText()
         }
-        val test = gson.fromJson(jsonString, Quiz::class.java)
+        val type = object : TypeToken<List<Question>>() { }.type
+        val questions = gson.fromJson<List<Question>>(jsonString, type)
         //get name, height,weight --> calc bmi
+        Log.d(TAG, "onCreate: $questions")
+        var score = 0
+        var quiz = Quiz(questions)
+    }
+
+    private fun wireWidgets(){
+        questions=findViewById(R.id.questions)
+        option1=findViewById(R.id.optionA)
+        option2=findViewById(R.id.optionB)
+        option3=findViewById(R.id.optionC)
+        option4=findViewById(R.id.optionD)
     }
 }
--- app/src/main/java/com/example/quizapp/Quiz.kt
+++ app/src/main/java/com/example/quizapp/Quiz.kt
@@ -1,9 +1,7 @@
 package com.example.quizapp
 
-class Quiz (
-    val question: String,
-    val choices: List<Choices>
-)
-data class Choices(
-    val a: Int
-)
+class Quiz (val questions: List<Question>) {
+
+
+
+}
