cdb4bd2737ba7e85cd0437296944d8ec6d0ea5a1
Niki <nikic5409@gmail.com>
2025-10-18 20:20:29 -0700

    finished Quiz.kt?

--- app/src/main/java/com/example/quizapp/Quiz.kt
+++ app/src/main/java/com/example/quizapp/Quiz.kt
@@ -7,15 +7,34 @@ class Quiz (val questions: List<Question>) {
     // these are the questions that we need methods to answer
     // are there more questions?
     fun hasMoreQuestions() : Boolean {
+        return currentQuestionIndex < questions.size - 1
         // since you are deleting the question from the list as we go, you can just see if it's empty or not
         // also we need to make the list Mutable
     }
+    
+    fun getCurrentQuestion(): Question {
+        return questions[currentQuestionIndex]
+    }
+
+    fun getCurrentQuestionNumber(): Int {
+        return currentQuestionIndex + 1
+    }
+
     // what is the current question?
     // is the answer they selected correct?
 
-    fun questionTracker(){
-        //after 1st go ++
-        //keep going till no ++
+    fun getTotalQuestions(): Int {
+        return questions.size
+    }
+    
+    fun addPoints(points: Int) {
+        score += points
+    }
+    
+    fun nextQuestion() {
+        if (hasMoreQuestions()) {
+            currentQuestionIndex++
+        }
     }
 
 }
